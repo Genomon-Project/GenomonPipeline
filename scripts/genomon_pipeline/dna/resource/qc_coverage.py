e527cbb89a9d23c3c20634cf2ab9a9b801e10f0f
aokad <aokada@ncc.go.jp>
2020-03-30 19:46:31 +0900

    add grc-flag

--- scripts/genomon_pipeline/dna/resource/qc_coverage.py
+++ scripts/genomon_pipeline/dna/resource/qc_coverage.py
@@ -26,11 +26,11 @@ export samtools=/usr/local/bin/samtools
 if [ "{data_type}" = "wgs" ]
 then
 ########## WGS ##########
-genomon_qc wgs {input_file} {output_file} --genome_size_file {genome_size_file} --gaptxt {gaptxt} --incl_bed_width {incl_bed_width} --i_bed_lines {i_bed_lines} --i_bed_width {i_bed_width} --bedtools $bedtools --samtools $samtools --samtools_params "{samtools_params}" --coverage_text {coverage_text} --del_tempfile
+genomon_qc wgs {input_file} {output_file} --genome_size_file {genome_size_file} --gaptxt {gaptxt} --incl_bed_width {incl_bed_width} --i_bed_lines {i_bed_lines} --i_bed_width {i_bed_width} --bedtools $bedtools --samtools $samtools --samtools_params "{samtools_params}" --coverage_text {coverage_text} {grc_flag} --del_tempfile
 
 else
 ########## exome ##########
-genomon_qc exome {input_file} {output_file} --bait_file {bait_file} --bedtools $bedtools --samtools $samtools --samtools_params "{samtools_params}" --coverage_text {coverage_text} --del_tempfile
+genomon_qc exome {input_file} {output_file} --bait_file {bait_file} --bedtools $bedtools --samtools $samtools --samtools_params "{samtools_params}" --coverage_text {coverage_text} {grc_flag} --del_tempfile
 fi
 """
 
@@ -50,6 +50,10 @@ def configure(input_bams, genomon_conf, run_conf, sample_conf):
     data_type = "exome"
     if genomon_conf.get("qc_coverage", "wgs_flag") == "True":
         data_type = "wgs"
+    
+    grc_flag = ""
+    if genomon_conf.get("qc_coverage", "grc_flag") == "True":
+        grc_flag = "--grc_flag"
 
     output_files = []
     for sample in sample_conf.qc:
@@ -66,6 +70,7 @@ def configure(input_bams, genomon_conf, run_conf, sample_conf):
             "gaptxt": genomon_conf.path_get("qc_coverage", "gaptxt"),
             "bait_file": genomon_conf.path_get("qc_coverage", "bait_file"),
             "samtools_params": genomon_conf.get("qc_coverage", "samtools_params"),
+            "grc_flag": grc_flag,
             "input_file": input_bams[sample],
             "output_file": "%s/%s" % (run_conf.project_root, output_file)
         }
